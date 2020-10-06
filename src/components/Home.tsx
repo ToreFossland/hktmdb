9e1305b09607f8428371a02bf37900d3702dcc89
Haakon Gunnarsli <haakgu@stud.ntnu.no>
2020-10-06 11:06:26 +0200

    initialisering av prosjekt

---
+++ src/components/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+
+const Home = () => {
+    return (
+      <div>
+          <p>hei!</p>
+      </div>
+    );
+  }
+  
+  export default Home;
