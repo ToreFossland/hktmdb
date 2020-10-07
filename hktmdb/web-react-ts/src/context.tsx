8f5c122e4a432e8c67bf0ac9970d4b46a0f64aef
Haakon Gunnarsli <haakgu@stud.ntnu.no>
2020-10-07 12:36:23 +0200

    init prosject #issue 1

---
+++ hktmdb/web-react-ts/src/context.tsx
@@ -0,0 +1,24 @@
+import { useLocalObservable} from 'mobx-react-lite';
+//useLocalStore is deprecated and replaced by useLocalObservable
+import React from 'react';
+
+import { createStore, TStore } from './store';
+
+
+
+const StoreContext = React.createContext<TStore | null>(null);
+
+export const DataStoreProvider = ({ children }: any) => {
+    //Vi gjør store observable
+    const store = useLocalObservable(createStore);
+    return <StoreContext.Provider value={store}> {children} </StoreContext.Provider>;
+};
+
+export const useDataStore = () => {
+    const store = React.useContext(StoreContext);
+    if (!store) {
+        throw new Error('useStore must be used within a StoreProvider.');
+    }
+    return store;
+}
+
---
+++ hktmdb/web-react-ts/src/store.ts
@@ -0,0 +1,16 @@
+
+
+
+
+export function createStore() {
+    return {
+        data: [] as string[],
+        addData(item: string) {
+        this.data.push(item);
+        },
+        removeData(item: string){
+        this.data.splice(this.data.indexOf(item), 1)
+        },
+    };
+}
+    export type TStore = ReturnType<typeof createStore>;
